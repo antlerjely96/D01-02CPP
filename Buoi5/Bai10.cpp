79c61faffc2376767d4f4eb957975054a23b0684
antlerjely96 <antlerjely96@gmail.com>
2025-10-27 16:14:28 +0700

    demo Loop Bai10

---
+++ Buoi5/Bai10.cpp
@@ -0,0 +1,22 @@
+/*
+	10. Ve hinh
+	* * * * *
+	* * * * *
+	* * * * *
+	* * * * *
+	* * * * *
+	Goi y: dung nested loop: i: 1 -> 5, j: 1 -> 5
+*/
+
+#include <iostream>
+using namespace std;
+
+int main(){
+	for ( int i = 1; i <= 5; i++ ){
+		for ( int j = 1; j <= 5; j++ ){
+			cout << " * ";
+		}
+		cout << endl;
+	}
+	return 0;
+}
