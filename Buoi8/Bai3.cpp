d5824f6ed0e028dea284b624f55505149bf40290
antlerjely96 <antlerjely96@gmail.com>
2025-11-24 15:07:19 +0700

    demo Mảng (array) Bai3

---
+++ Buoi8/Bai3.cpp
@@ -0,0 +1,20 @@
+/*
+	3. Nhap mang so thuc co n phan tu, n nhap tu ban phim.
+	Tinh tong gia tri cua cac phan tu trong mang.
+*/
+#include <iostream>
+using namespace std;
+int main(){
+	int n;
+	cout << "n = ";
+	cin >> n;
+	int arr[n];
+	int tong = 0;
+	for(int i = 0; i < n; i++){
+		cout << "arr[" << i << "] =" << "";
+		cin >> arr[i];
+		tong += arr[i];
+	}
+	cout << "Tong cac gia tri phan tu la =" << tong << endl;
+	return 0;
+}
