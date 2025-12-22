4ce1e3ecb2240ff6ba408a28069cb2578b4d3da8
antlerjely96 <antlerjely96@gmail.com>
2025-12-22 16:25:43 +0700

    demo Function Bai17, 18, 19

--- Buoi13/Bai17.cpp
+++ Buoi13/Bai17.cpp
@@ -2,3 +2,34 @@
 	17. Nhap mang so thuc co n phan tu.
 	Dem so phan tu co gia tri duong trong mang.
 */
+
+#include <iostream>
+using namespace std;
+
+int nhapN(){
+	int n;
+	cin >> n;
+	return n;
+}
+
+int demSoDuong(int n, int arr[]){
+	int dem = 0;
+	for(int i = 0; i < n; i++){
+		if(arr[i] > 0){
+			dem++;
+		}
+	}
+	return dem;
+}
+
+int main(){
+	cout << "n = ";
+	int n = nhapN();
+	int arr[n];
+	for(int i = 0; i < n; i++){
+		cout << "arr[" << i << "] = ";
+		cin >> arr[i];
+	}
+	cout << "So luong phan tu duong trong mang: " << demSoDuong(n, arr) << endl;
+	return 0;
+}
--- Buoi13/Bai18.cpp
+++ Buoi13/Bai18.cpp
@@ -3,3 +3,34 @@
 	Nhap so nguyen x tu ban phim.
 	Dem so lan x xuat hien trong mang
 */
+
+#include <iostream>
+using namespace std;
+
+int nhap(){
+	int n;
+	cin >> n;
+	return n;
+}
+
+int demSoLanXuatHien(int n, int arr[], int x){
+	int dem = 0;
+	for(int i = 0; i < n; i++){
+		if(arr[i] == x){
+			dem++;
+		}
+	}
+	return dem;
+}
+
+int main(){
+	cout << "n = ";
+	int n = nhap();
+	for(int i = 0; i < n; i+){
+		cout << "arr[" << i << "] = ";
+		cin >> arr[i];
+	}
+	cout << "x = ";
+	int x = nhap();
+	cout << "So lan xua hien cua x trong mang: " << demSoLanXuatHien(n, arr, x) << endl;
+}
