aab9e4b47d762698c2231f901129ed32cf024185
antlerjely96 <antlerjely96@gmail.com>
2025-12-22 17:20:59 +0700

    BTVN Function Bai20, 21, 22, 23

---
+++ Buoi13/Bai20.cpp
@@ -0,0 +1,11 @@
+/*
+	20. Nhap mang so thuc co n phan tu.
+	Sap xep mang theo chieu tang dan
+	21. Nhap mang so nguyen co n phan tu.
+	Tim phan tu co gia tri lon thu 2 trong mang
+	22. Nhap mang so nguyen co n phan tu.
+	Nhap 2 so nguyen m, k.
+	Chen gia tri m vao vi tri co index = k trong mang
+	23. Nhap mang so nguyen co n phan tu.
+	Hien thi tat ca cac phan tu la so nguyen to trong mang
+*/
