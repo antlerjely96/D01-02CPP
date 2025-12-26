448da77f073882d32ec37c8ed384f15594b10ac4
antlerjely96 <antlerjely96@gmail.com>
2025-12-26 08:55:17 +0700

    Function Bai20

--- Buoi13/Bai20.cpp
+++ Buoi13/Bai20.cpp
@@ -1,11 +1,48 @@
 /*
 	20. Nhap mang so thuc co n phan tu.
 	Sap xep mang theo chieu tang dan
-	21. Nhap mang so nguyen co n phan tu.
-	Tim phan tu co gia tri lon thu 2 trong mang
-	22. Nhap mang so nguyen co n phan tu.
-	Nhap 2 so nguyen m, k.
-	Chen gia tri m vao vi tri co index = k trong mang
-	23. Nhap mang so nguyen co n phan tu.
-	Hien thi tat ca cac phan tu la so nguyen to trong mang
 */
+
+#include <iostream>
+using namespace std;
+
+int nhap(){
+	int n;
+	cin >> n;
+	return n;
+}
+
+void hienThi(int n, double arr[]){
+	for(int i = 0; i < n; i++){
+		cout << arr[i] << "\t";
+	}
+	cout << endl;
+	return;
+}
+
+void sapXep(int n, double arr[]){
+	double temp;
+	for(int i = 0; i < n - 1; i++){
+		for(int j = i + 1; j < n; j++){
+			if(arr[i] > arr[j]){
+				temp = arr[i];
+				arr[i] = arr[j];
+				arr[j] = temp;
+			}
+		}
+	}
+	hienThi(n, arr);
+	return;
+}
+
+int main(){
+	cout << "n = ";
+	int n = nhap();
+	double arr[n];
+	for(int i = 0; i < n; i++){
+		cout << "arr[" << i << "] = ";
+		cin >> arr[i];
+	}
+	sapXep(n, arr);
+	return 0;
+}
